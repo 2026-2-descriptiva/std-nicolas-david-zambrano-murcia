f49de0392ac88b16b6ad62e36badfa5739400496
Nicolas Murcia <159479780+Nsja2806@users.noreply.github.com>
2026-09-11 18:26:16 -0500

    Update_WordCount1 Sin testeo

---
+++ PRE_02_mapreduce/src/word_count_1.py
@@ -0,0 +1,96 @@
+import glob #Esta recibe una ruta y devuelve el contenido de la carpeta
+import os.path
+import string
+import time
+
+#Definición de variables
+DATA_FOLDER = "PRE_02_mapreduce/data"
+INPUT_FOLDER = "PRE_02_mapreduce/temp/input"
+OUTPUT_FOLDER = "PRE_02_mapreduce/temp/output"
+
+#Crear la carpeta y verificar que este vacia 
+if os.path.exists(INPUT_FOLDER):
+    for file in glob.glob(f"{INPUT_FOLDER}/*"):
+        os.remove(file)
+else:
+    os.makedirs(INPUT_FOLDER)
+
+#Creamos las copias de los archivos ubicados en data
+n = 1000 #Numero de copias por archivo
+
+for file in glob.glob(f"{DATA_FOLDER}/*"):
+
+    with open(file, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    for i in range(1, n + 1): #el +1 es por que el contador empieza en 0
+
+        raw_filename_with_extension = os.path.basename(file) #devuelve el nombre del archivo con extension
+
+        raw_filename_without_extension = os.path.splitext(raw_filename_with_extension)[
+            0
+        ]
+
+        new_filename = f"{raw_filename_without_extension}_{i:05d}.txt"
+
+        with open(f"{INPUT_FOLDER}/{new_filename}", "w", encoding="utf-8") as f2:
+            f2.write(text)
+            
+start_time = time.time()            
+sequence = []
+files = glob.glob(f"{INPUT_FOLDER}/*")
+for file in files:
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            sequence.append((file, line))
+                   
+#Mappper
+pairs_sequence = []
+for _, line in sequence:
+    line = line.lower()
+    line = line.translate(str.maketrans("", "", string.punctuation)) #Borra lo que se ponga en la def, en este caso la puntuación
+    line = line.replace("\n", "")
+    words = line.split()
+    pairs_sequence.extend([(word, 1) for word in words]) #imprimer las tuplas de las palabras, por palabra el valor es uno
+    
+# Shuffle and sort
+# -----------------------------------------------------------------------------
+
+pairs_sequence = sorted(pairs_sequence)     
+
+# Reducer
+# -----------------------------------------------------------------------------
+
+result = []
+for key, value in pairs_sequence:
+    if result and result[-1][0] == key:
+        result[-1] = (key, result[-1][1] + value)
+    else:
+        result.append((key, value))
+
+#Este modo de ordenar no deja los resultados en disco, sino en nube, por eso creamos archivo de output
+# La carpeta de salida debe estar vacia
+# -----------------------------------------------------------------------------
+
+if os.path.exists(OUTPUT_FOLDER):
+    for file in glob.glob(f"{OUTPUT_FOLDER}/*"):
+        os.remove(file)
+else:
+    os.makedirs(OUTPUT_FOLDER) #Es importante verficar si lo que entregamos áca es completo, quizá hubo un fallo en el preproceso
+    
+#Voy pegando los resultados a la carpeta que cree, el archivo con el conteo
+# -----------------------------------------------------------------------------
+
+with open(f"{OUTPUT_FOLDER}/part-00000", "w", encoding="utf-8") as f:
+    for key, value in result:
+        f.write(f"{key}\t{value}\n")
+
+# Marcador de éxito (verifica que si este completo)
+# -----------------------------------------------------------------------------
+with open(f"{OUTPUT_FOLDER}/_SUCCESS", "w", encoding="utf-8") as f:
+    f.write("")
+
+# Reporte de tiempo de ejecución
+# -----------------------------------------------------------------------------
+end_time = time.time()
+print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
